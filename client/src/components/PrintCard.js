f07249703011d953b83ff6d48a7ab0571e98ee6e
David Titarenco <david.titarenco@gmail.com>
2018-09-21 14:28:31 -0700

    layout stuff

--- client/src/components/PrintCard.js
+++ client/src/components/PrintCard.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
 import { Row, Col, Form, FormGroup, Button, FormControl, ControlLabel, Checkbox } from 'react-bootstrap';
 import Card from './Card'
+import CardBuilder from './CardBuilder'
 
 import './PrintCard.css';
 
 class PrintCard extends Component {
+
+    constructor() {
+        super()
+        this.card = { }
+    }
+
     render() {
         return(
             <Row className="main">
                 <Col lg={6} sm={12} className="left">
-                    <Card />
+                    <Card payload={this.card}/>
                 </Col >
                 <Col lg={6} sm={12} className="right">
-                    <form>
-                    <FormGroup bsSize="large">
-                        <FormControl type="text" placeholder="Large text" />
-                    </FormGroup>
-                    <FormGroup>
-                        <FormControl type="text" placeholder="Normal text" />
-                    </FormGroup>
-                    <FormGroup bsSize="small">
-                        <FormControl type="text" placeholder="Small text" />
-                    </FormGroup>
-                    </form>
+                    <CardBuilder payload={this.card}/>
                 </Col >
             </Row>
         );
